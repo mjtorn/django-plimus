5caea6fe7b54a9a1f7e1ca3040733b76e17e0e33
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-12-09 09:56:35 +0000

    Timestamp the IPNs

--- plimus/forms.py
+++ plimus/forms.py
@@ -4,16 +4,20 @@ from django import forms
 
 from plimus import models
 
+from datetime import datetime
+
 class PlimusBaseIPNForm(forms.Form):
     def save(self):
         """Return a list of created IPN attributes instead of single model
         """
 
+        now = datetime.now()
+
         refnum = self.data['referenceNumber']
 
         attrs = []
         for k, v in self.data.items():
-            model = self.model(key=k, value=v, reference_number=refnum)
+            model = self.model(key=k, value=v, reference_number=refnum, tstamp=now)
             model.save()
 
             attrs.append(model)
